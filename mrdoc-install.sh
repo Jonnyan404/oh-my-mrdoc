7f2f40d09a5e561f1fad937ff6245ee3de232d7e
jonny <475634114@qq.com>
2021-04-20 14:47:15 +0800

    add python3-dev package

--- mrdoc-install.sh
+++ mrdoc-install.sh
@@ -29,7 +29,7 @@ installdepend(){
         apt-get -qq update
         SOFTWARE_UPDATED=1
         fi
-        apt-get -y  install git pwgen python3 python3 python3-pip nginx 
+        apt-get -y  install git pwgen python3 python3-dev python3-pip nginx 
 
     elif [[ -n $(command -v yum) ]]; then
         if [[ $SOFTWARE_UPDATED -eq 0 ]]; then
