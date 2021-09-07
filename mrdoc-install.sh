22e010896c6410ef79ce3fa1e0cf86b94dd911db
jonny <475634114@qq.com>
2021-09-07 15:59:37 +0800

    Update mrdoc-install.sh

--- mrdoc-install.sh
+++ mrdoc-install.sh
@@ -29,8 +29,7 @@ installdepend(){
         apt-get -qq update
         SOFTWARE_UPDATED=1
         fi
-        apt-get -y  install git pwgen python3 python3-dev python3-pip nginx 
-
+        apt-get -y  install git pwgen python3 python3-dev python3-pip nginx gcc wheel python3-setuptools python3-venv
     elif [[ -n $(command -v yum) ]]; then
         if [[ $SOFTWARE_UPDATED -eq 0 ]]; then
         colorEcho ${BLUE} "Updating software repo"
