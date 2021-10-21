a953f081786e34b5c164918cc03c93ce9460c005
jonny <475634114@qq.com>
2021-10-21 13:20:34 +0800

    兼容专业版

--- mrdoc.sh
+++ mrdoc.sh
@@ -26,10 +26,10 @@ installdepend(){
     if [[ -n $(command -v apt-get) ]];then
         if [[ $SOFTWARE_UPDATED -eq 0 ]]; then
         colorEcho ${BLUE} "Updating software repo"
-        apt-get -qq update
+        apt-get  update
         SOFTWARE_UPDATED=1
         fi
-        apt-get -y  install git pwgen python3 python3-dev python3-pip gcc wheel python3-setuptools python3-venv
+        apt-get -y  install git pwgen python3 python3-dev python3-pip gcc python3-wheel python3-setuptools python3-venv
     elif [[ -n $(command -v yum) ]]; then
         if [[ $SOFTWARE_UPDATED -eq 0 ]]; then
         colorEcho ${BLUE} "Updating software repo"
@@ -69,35 +69,35 @@ installmrdoc(){
     MM=$(pwgen -1s)
 
     mkdir -p ${MRDOCDIR}
-    touch ${MRDOCDIR}/pwdinfo.log
-    cd ${MRDOCDIR} && git clone https://gitee.com/zmister/MrDoc.git
+    touch ${MRDOCDIR}/"${GIT_DIR}"pwdinfo.log
+    cd ${MRDOCDIR} && git clone ${GIT_LINK}
     pip3 install uwsgi
-    python3 -m venv ${MRDOCDIR}/mrdoc_env
-    source ${MRDOCDIR}/mrdoc_env/bin/activate \
+    python3 -m venv ${MRDOCDIR}/"${GIT_DIR}"_env
+    source ${MRDOCDIR}/"${GIT_DIR}"_env/bin/activate \
     && pip install --upgrade pip \
-    && cd ${MRDOCDIR}/MrDoc \
+    && cd ${MRDOCDIR}/"${GIT_DIR}" \
     && pip3 install -r requirements.txt \
     && python3 manage.py makemigrations \
     && python3 manage.py migrate \
-    && if echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${USER}', 'www@mrdoc.fun', '${MM}')" | python manage.py shell;then printf "$(date) user:%s pwd:%s\n" "$USER" "$MM" >>/opt/jonnyan404/pwdinfo.log; fi \
+    && if echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${USER}', 'www@mrdoc.fun', '${MM}')" | python manage.py shell;then printf "$(date) user:%s pwd:%s\n" "$USER" "$MM" >>/opt/jonnyan404/"${GIT_DIR}"pwdinfo.log; fi \
     && deactivate
 }
 
 initconfig(){
 ###  Generate uwsgi configuration file ###
-    if [[ ! -f "/opt/jonnyan404/mrdoc_uwsgi.ini" ]]; then
-cat >"/opt/jonnyan404/mrdoc_uwsgi.ini"<<EOF
+    if [[ ! -f "/opt/jonnyan404/${GIT_DIR}_uwsgi.ini" ]]; then
+cat >"/opt/jonnyan404/${GIT_DIR}_uwsgi.ini"<<EOF
 [uwsgi]
 
 # Django-related settings
-http            = :${port:-10086}
+http            = :${mrdocport:-10086}
 # the base directory (full path)
-chdir           = /opt/jonnyan404/MrDoc
+chdir           = /opt/jonnyan404/${GIT_DIR}
 # Django's wsgi file
-module          = MrDoc.wsgi:application
-wsgi-file       = MrDoc/wsgi.py
+module          = ${GIT_DIR}.wsgi:application
+wsgi-file       = ${GIT_DIR}/wsgi.py
 # the virtualenv (full path)
-home            = /opt/jonnyan404/mrdoc_env
+home            = /opt/jonnyan404/${GIT_DIR}_env
 
 # process-related settings
 # master
@@ -105,7 +105,7 @@ master          = true
 # maximum number of worker processes
 processes       = 5
 # the socket (use the full path to be safe
-socket          = /opt/jonnyan404/mrdoc.sock
+socket          = /opt/jonnyan404/${GIT_DIR}.sock
 # ... with appropriate permissions - may be needed
 chmod-socket    = 666
 # clear environment on exit
@@ -113,31 +113,31 @@ vacuum          = true
 EOF
     fi
 ### Generate nginx configuration file ###
-    if [[ ! -f "/opt/jonnyan404/mrdoc_nginx_jonnyan404.conf" ]]; then
-cat >"/opt/jonnyan404/mrdoc_nginx_jonnyan404.conf"<<EOF
+    if [[ ! -f "/opt/jonnyan404/${GIT_DIR}_nginx_jonnyan404.conf" ]]; then
+cat >"/opt/jonnyan404/${GIT_DIR}_nginx_jonnyan404.conf"<<EOF
 server {
     # the port your site will be served on
     listen      80;
     # the domain name it will serve for
     server_name _; # substitute your machine's IP address or FQDN
     charset     utf-8;
-    access_log /var/log/nginx/mrdoc-nginx-access.log;
-    error_log  /var/log/nginx/mrdoc-nginx-error.log;
+    access_log /var/log/nginx/${GIT_DIR}-nginx-access.log;
+    error_log  /var/log/nginx/${GIT_DIR}-nginx-error.log;
     # max upload size
     client_max_body_size 75M;   # adjust to taste
 
     # Django media
     location /media  {
-        alias /opt/jonnyan404/MrDoc/media;  # your Django project's media files - amend as required
+        alias /opt/jonnyan404/${GIT_DIR}/media;  # your Django project's media files - amend as required
     }
 
     location /static {
-        alias /opt/jonnyan404/MrDoc/static; # your Django project's static files - amend as required
+        alias /opt/jonnyan404/${GIT_DIR}/static; # your Django project's static files - amend as required
     }
 
     # Finally, send all non-media requests to the Django server.
     location / {
-        uwsgi_pass  unix:///opt/jonnyan404/mrdoc.sock;
+        uwsgi_pass  unix:///opt/jonnyan404/${GIT_DIR}.sock;
         include     /etc/nginx/uwsgi_params; # the uwsgi_params file you installed
     }
 }
@@ -145,7 +145,7 @@ EOF
     fi
 ### Generate systemd configuration file ###
 if [[ -n "${SYSTEMCTL_CMD}" ]];then
-cat>"/opt/jonnyan404/mrdocfun.service"<<EOF
+cat>"/opt/jonnyan404/${GIT_DIR}fun.service"<<EOF
 [Unit]
 Description=mrdoc service by jonnyan404
 Requires=network.target
@@ -162,7 +162,7 @@ NotifyAccess=all
 StandardError=syslog
 RuntimeDirectory=uwsgi
 # Main call: Virtual env is activated and uwsgi is started with INI file as argument
-ExecStart=/bin/bash -c 'cd /opt/jonnyan404/; source mrdoc_env/bin/activate; uwsgi --ini /opt/jonnyan404/mrdoc_uwsgi.ini'
+ExecStart=/bin/bash -c 'cd /opt/jonnyan404/; source ${GIT_DIR}_env/bin/activate; uwsgi --ini /opt/jonnyan404/${GIT_DIR}_uwsgi.ini'
 
 [Install]
 WantedBy=multi-user.target
@@ -175,36 +175,40 @@ return
 
 start(){
     systemctl daemon-reload
-    if systemctl start mrdocfun;then
-        return 0
+    if systemctl start "${GIT_DIR}"fun;then
+        colorEcho  ${GREEN} "启动${GIT_DIR}成功"
     else
-        return 1
+        systemctl status "${GIT_DIR}"fun
+        colorEcho  ${RED} "启动${GIT_DIR}失败"
     fi
 }
 
 stop(){
     systemctl daemon-reload
-    if systemctl stop mrdocfun;then
-        return 0
+    if systemctl stop "${GIT_DIR}"fun;then
+        colorEcho  ${GREEN} "停止${GIT_DIR}成功"
     else
-        return 1
+        systemctl status "${GIT_DIR}"fun
+        colorEcho  ${RED} "停止${GIT_DIR}失败"
     fi
 }
 
 restart(){
     systemctl daemon-reload
-    if systemctl restart mrdocfun;then
-        return 0
+    if systemctl restart "${GIT_DIR}"fun;then
+        colorEcho  ${GREEN} "重启${GIT_DIR}成功"
     else
-        return 1
+        systemctl status "${GIT_DIR}"fun
+        colorEcho  ${RED} "重启${GIT_DIR}失败"
     fi
 }
 
 remove(){
-    echo "执行卸载中(卸载采用软删除,彻底删除请到/tmp目录手动删除!),请等待..."
-    systemctl stop mrdocfun
-    mv -f /opt/jonnyan404 /tmp/
-    mv -f /etc/systemd/system/mrdocfun.service /tmp/
+    echo "执行卸载中(卸载采用软删除,彻底删除请到/tmp目录手动删除!)"
+    systemctl stop "${GIT_DIR}"fun
+    cp -rf /opt/jonnyan404 /tmp
+    rm -rf /opt/jonnyan404
+    rm -f /etc/systemd/system/"${GIT_DIR}"fun.service
     systemctl daemon-reload
     colorEcho  ${GREEN} "卸载完成!"
 
@@ -212,9 +216,9 @@ remove(){
 
 update(){
     MRDOCDIR=/opt/jonnyan404
-    cd ${MRDOCDIR}/MrDoc && git pull
-    source ${MRDOCDIR}/mrdoc_env/bin/activate \
-    && cd ${MRDOCDIR}/MrDoc \
+    cd ${MRDOCDIR}/"${GIT_DIR}" && git pull
+    source ${MRDOCDIR}/"${GIT_DIR}"_env/bin/activate \
+    && cd ${MRDOCDIR}/"${GIT_DIR}" \
     && pip3 install -r requirements.txt \
     && python3 manage.py makemigrations \
     && python3 manage.py migrate \
@@ -236,21 +240,30 @@ Help(){
     return 0
 }
 
+
 main(){
     if installdepend ;then
-        colorEcho  ${BLUE}  "###安装mrdoc中...###"
+        if [[ $(echo $GIT_LINK | grep "git.mrdoc.pro/MrDoc/MrDocPro") != "" ]];then
+            GIT_DIR=$(echo $GIT_LINK|grep -e "MrDocPro" -o)
+            echo $GIT_DIR
+            colorEcho  ${BLUE}  "###安装 mrdoc 专业版中...###"
+        else
+            GIT_LINK="https://gitee.com/zmister/MrDoc.git"
+            GIT_DIR=$(echo $GIT_LINK|grep -e "MrDoc"  -o)
+            colorEcho  ${BLUE}  "###安装 mrdoc 开源版中...###"
+        fi
         installmrdoc
         colorEcho  ${BLUE}  "###初始化配置中...###"
         initconfig
-        ln -sf /opt/jonnyan404/mrdocfun.service /etc/systemd/system/mrdocfun.service
+        ln -sf /opt/jonnyan404/"${GIT_DIR}"fun.service /etc/systemd/system/"${GIT_DIR}"fun.service
         colorEcho  ${BLUE}  "###启动mrdoc...###"
         if start;then
-            systemctl status mrdocfun
-            colorEcho  ${GREEN}  "$(cat /opt/jonnyan404/pwdinfo.log),Password is saved in /opt/jonnyan404/pwdinfo.log"
+            systemctl status "${GIT_DIR}"fun
+            colorEcho  ${GREEN}  "$(cat /opt/jonnyan404/${GIT_DIR}pwdinfo.log),Password is saved in /opt/jonnyan404/${GIT_DIR}pwdinfo.log"
             colorEcho  ${GREEN}  "如果上方没显示账号密码,就是部署失败,请进群\@亖\反馈!QQ群号:735507293"
         else
             colorEcho  ${RED}  "部署失败,请进群\@亖\反馈!QQ群号:735507293"
-            systemctl status mrdoc
+            systemctl status "${GIT_DIR}"
         fi
     fi
 }
@@ -263,16 +276,35 @@ while [[ $# -gt 0 ]];do
         Help
         ;;
         -i|--install)
+        GIT_LINK=$2
         main
-        #shift # past argument
+        shift # past argument
         ;;
         -start|--start)
+        if [[ "$2" == "pro" ]] ;then
+            GIT_DIR=MrDocPro
+            shift
+        else
+            GIT_DIR=MrDoc
+        fi
         start
         ;;
         -stop|--stop)
+        if [[ "$2" == "pro" ]] ;then
+            GIT_DIR=MrDocPro
+            shift
+        else
+            GIT_DIR=MrDoc
+        fi
         stop
         ;;
         -restart|--restart)
+        if [[ "$2" == "pro" ]] ;then
+            GIT_DIR=MrDocPro
+            shift
+        else
+            GIT_DIR=MrDoc
+        fi
         restart
         ;;
         -v|--version)
@@ -280,9 +312,21 @@ while [[ $# -gt 0 ]];do
         #shift
         ;;
         -u|--update)
+        if [[ "$2" == "pro" ]] ;then
+            GIT_DIR=MrDocPro
+            shift
+        else
+            GIT_DIR=MrDoc
+        fi
         update
         ;;
         --remove)
+        if [[ "$2" == "pro" ]] ;then
+            GIT_DIR=MrDocPro
+            shift
+        else
+            GIT_DIR=MrDoc
+        fi
         remove
         ;;
         -c|--check)
